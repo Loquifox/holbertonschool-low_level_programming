996caf18183e02d26347caadd11011b289319699
Loquifox <lucasgarridosape@gmail.com>
2022-09-28 10:02:15 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - void
+ *
+ * Description: printf in c
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return 0;
+}
+
