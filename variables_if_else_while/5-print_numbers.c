bdbf16b4a458251dec5a1fe69197d51be9984823
Loquifox <lucasgarridosape@gmail.com>
2022-10-05 06:43:22 -0700

    everything

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}i
