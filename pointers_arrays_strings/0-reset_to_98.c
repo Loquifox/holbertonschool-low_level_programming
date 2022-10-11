0d419122f61584e88d176080c280c6effc78196c
Loquifox <lucasgarridosape@gmail.com>
2022-10-11 10:18:45 -0700

    task

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - updates value
- *
+ * reset_to_98 - sets value to 98
+ * @n: address of value
  *
  * Return: 0
  */
 
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
-	
